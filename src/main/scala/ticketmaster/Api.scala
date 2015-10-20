9aebc9c3240cf5d4c4d596881e558af64daaef8a
Keshi Dai <keshi.dai@gmail.com>
2015-10-19 21:49:01 -0400

    Remove useless code

--- src/main/scala/ticketmaster/Api.scala
+++ src/main/scala/ticketmaster/Api.scala
@@ -3,17 +3,19 @@ package ticketmaster
 import dispatch._
 
 import scala.concurrent.ExecutionContext
-import scala.util.{Failure, Success}
+import scala.util.{Success, Failure}
 
 object TicketmasterConstant {
   final val header= Map(
     "User-Agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.71 Safari/537.36",
-    "Content-Type" -> "application/json"
+    "Accept" -> "application/json"
   )
 
   final val commonReqParaMap = Map(
     "apiKey" -> "17b528e219698770b8e4ab2713c74df6",
-    "country" -> "US"
+    "country" -> "US",
+    "filter.venue.state" -> "NY",
+    "filter.venue.city" -> "New York"
   )
 
   final val baseUrl = "http://ticketmaster.productserve.com/v3/event"
@@ -25,33 +27,36 @@ object Api {
 
   def buildParameterMap(parameters: Map[String, String]) = commonReqParaMap ++ parameters
 
-  def buildRequest(parameters: Map[String, String], userAgent: Map[String, String]): Req = {
-    url(baseUrl).GET <<? buildParameterMap(parameters) <:< userAgent
+  def buildRequest(parameters: Map[String, String], header: Map[String, String]): Req = {
+    url(baseUrl).GET <<? buildParameterMap(parameters) <:< header
   }
 
-  val requestUrl = buildRequest(
-    Map("currentPage" -> "1", "resultsPerPage" -> "1", "filter.venue.state" -> "NY"),
+  def buildRequest(curPage: Int, resultsPerPage: Int): Req = buildRequest(
+    Map("currentPage" -> curPage.toString, "resultsPerPage" -> resultsPerPage.toString),
     header
   )
 
-  println(requestUrl.url)
+  def queryTicketmaster(): Unit = {
+    implicit val ec = ExecutionContext.Implicits.global
 
-  def getNewYorkEvents(implicit ec: ExecutionContext) = {
-    val response = Http(requestUrl OK as.json4s.Json).map { json ⇒ json.extract[TicketmasterResponse] }
+    val request = buildRequest(1, 10)
+    val response = Http(request OK as.json4s.Json).map(json ⇒ json.extract[TicketmasterResponse])
 
     response onComplete {
       case Success(res) => {
         val results = res.results
+        //logging to
         println(results)
       }
       case Failure(e) => {
         println("An error has occurred: " + e.getMessage)
       }
     }
+//    val results = response().results
+//    println(results)
   }
 
   def main(args: Array[String]) = {
-    implicit val ec = ExecutionContext.Implicits.global
-    getNewYorkEvents
+    queryTicketmaster()
   }
 }
